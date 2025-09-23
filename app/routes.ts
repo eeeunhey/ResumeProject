08f459cc4d4e86a2fd0820c50b31e7015ece836e
eunhye <dmsgp2627@naver.com>
2025-09-23 23:19:01 +0900

    home.tsx 수정

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx")] satisfies RouteConfig;
+//경로 기입
+//index("routes/home.tsx") → / (루트 경로)로 들어왔을 때 
+// routes/home.tsx 페이지를 보여줌. 홈 페이지 경로 지정.
+
