a06bba443cf5770544c6a2515693bf2974128588
eunhye <dmsgp2627@naver.com>
2025-10-05 20:13:28 +0900

    FE_로그인 문구 추가

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -10,7 +10,10 @@ const auth = () => {
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center ">
         <div className="gradient-border shadow-lg">
             <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
-
+            <div className="flex flex-col items-center gap-2 text-center">
+              <h1> 환영합니다! </h1>
+              <h2>로그인하고 당신의 커리어 여정을 이어가세요</h2>
+            </div>
                 
             </section>
 
