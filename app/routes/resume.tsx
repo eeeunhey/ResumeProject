6925b52347acdd17fdd07117b70a28ac846fbfd1
eunhye <dmsgp2627@naver.com>
2025-11-19 20:37:55 +0900

    bg 수정, ScoreCircle 수정

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,12 +1,20 @@
 import { useParams } from "react-router";
-
+import { Link } from "react-router";
+export const meta = () => [
+  { title: "Resumind | Review" },
+  { name: "description", content: "이력서를 기반으로 한 상세 요약입니다 " },
+];
 
 const resume = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-  return (
-    <div>resume {id} </div>
-  )
-}
+  return <main className="!pt-0">
+    <nav className="resume-nav">
+        <Link to="/" className="back-button">
+        </Link>
+    </nav>
 
-export default resume
+  </main>;
+};
+
+export default resume;
